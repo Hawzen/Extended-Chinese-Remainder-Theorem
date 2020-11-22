982a5ad6d3be3e185516ce47c15c255df19882b2
Hawzen <43524721+Hawzen@users.noreply.github.com>
2020-11-23 01:15:35 +0300

    Add testing

--- Main.hs
+++ Main.hs
@@ -13,7 +13,7 @@ main = do
     if coprimes ms 
         then let summation = sum $ map (solutionTerm bigM) eqs
                  ans = summation `mod` bigM
-                 bs = map (\(a, m) -> a * ans `rem` m) $ zip as ms
+                 bs = map (\(a, m) -> a * ans `mod` m) $ zip as ms
                  checks = map (\(a, m, b) -> 
                     "\t" ++ (show ans) ++ " * " ++ (show a) ++ " mod " ++ (show m) ++ " = " ++ (show b) ++ "\n"
                     ) $ zip3 as ms bs
@@ -21,15 +21,19 @@ main = do
                     putStrLn $ "X: " ++ (show ans)
                     putStrLn "Checks:"
                     putStrLn $ mconcat checks
-                    putStrLn "Again? (y\\n): "
-                    again <- getLine
-                    if ('y' `elem` again) then main else return ()
-        else putStrLn "Error: No solution (modulos are not coprimes)"
+                    again
+        else do
+            putStrLn "Error: No solution (modulos are not coprimes)"
+            again
 
+-- again :: IO
+again = do
+    putStrLn "Again? (y\\n): "
+    again <- getLine
+    if ('y' `elem` again) then main else return ()
 
 type Modulo = Integer
 data Equation = Equation { a :: Integer
-                         , x :: Integer
                          , b :: Integer
                          , m :: Modulo
                          } deriving (Eq, Show)
@@ -40,10 +44,14 @@ getEquation = do
             hFlush stdout
             nums <- getLine
             let (a:b:m:[]) = map read $ words nums
-            return (check (Equation a (-1) b m))
+                a':b':[] = map (formatter m) [a, b]
+            return (check (Equation a' b' m))
+          where
+            formatter m x = x `mod` m
 
 check :: Equation -> Equation
-check eq@(Equation a _ _ m) 
+check eq@(Equation a _ m) 
+    | m <= 1 = error "m is <= 1: "
     | coprime a m = eq
     | otherwise = error $ "Coprime Error: " ++ (show a) ++ " And " ++ (show m) ++ " are not coprime"
 
@@ -56,21 +64,19 @@ getEquations = do
         where
             getInt = fmap read getLine
 
+gcd' :: Integer -> Integer -> Integer
+gcd' a 0 = a
+gcd' a b = let (q, r) = a `divMod` b
+           in gcd' b r
+
 coprime :: Integer -> Integer -> Bool
 coprime a b = gcd' a b == 1
 
--- Pairwise co-primality
 coprimes :: [Modulo] -> Bool
 coprimes [] = True
 coprimes [x] = True
 coprimes (x:xs) = and (map (coprime x) xs)
                   && coprimes xs
-
-
-gcd' :: Integer -> Integer -> Integer
-gcd' a 0 = a
-gcd' a b = let (q, r) = a `divMod` b
-           in gcd' b r
 
 calcMs :: [Modulo] -> (Modulo, [Modulo])
 calcMs ms  = let bigM = product ms
@@ -80,11 +86,10 @@ calcMs ms  = let bigM = product ms
 inverse :: Integer -> Modulo -> Integer
 inverse a m = let condition x = a * x `mod` m == 1
                   list = filter condition [1..m]
-              in if null list then error "No inverse [EDIT ME LATER]"
-                              else head list 
+              in head list 
 
 solutionTerm :: Modulo -> Equation -> Integer
-solutionTerm bigM (Equation a x b m) = 
+solutionTerm bigM (Equation a b m) = 
         let a' = inverse a m
             bigMi = div bigM m
             y = inverse bigMi m
@@ -98,5 +103,4 @@ solutionTerm bigM (Equation a x b m) =
     * Is 0 allowed?
     * Inverse of 1 mod 1
     * Negative numbers allowed?
-4- Build is for windows 64 bit
 -}
